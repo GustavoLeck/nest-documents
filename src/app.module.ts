36c268f015581206e4280aab077c82c73de49b0d
Gustavo Leck <gustavoleck@hotmail.com>
2025-11-10 13:40:20 -0300

    feat/ Inicia estrutura de testes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EmployeModule } from './employe/employe.module';
 import { DocumentTypeModule } from './document-type/documente-type.module';
 import { DocumentModule } from './document/document-type/documente.module';
 
 @Module({
   imports: [EmployeModule, DocumentTypeModule, DocumentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
